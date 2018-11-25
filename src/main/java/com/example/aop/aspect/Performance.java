547afc9e25c6711867eb531ed9d1adcc25495d14
Kang Hoon <skyzzango@naver.com>
2018-11-25 15:13:35 +0900

    master(#8) - AOP 사용법 확장
     # UserService 에도 AOP 적용
      - 포인트컷 표현식에는 AND, OR, NOT와 같은 관계연산자를 이용할 수 있다.
      - @Pointcut 어노테이션은 애스펙트에서 마치 변수와 같이 재사용 가능한 포인트컷을 정의할 수 있다.

--- src/main/java/com/example/aop/aspect/Performance.java
+++ src/main/java/com/example/aop/aspect/Performance.java
@@ -3,13 +3,20 @@ package com.example.aop.aspect;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class Performance {
 
-	@Around("execution(* com.example.aop.board.service.BoardService.getBoards(..))")
+	@Pointcut("execution(* com.example.aop.board.service.BoardService.getBoards(..))")
+	public void getBoards(){}
+
+	@Pointcut("execution(* com.example.aop.user.service.UserService.getUsers(..))")
+	public void getUsers(){}
+
+	@Around("getBoards() || getUsers()")
 	public Object calculatePerformanceTime(ProceedingJoinPoint proceedingJoinPoint) {
 		Object result = null;
 		try {
